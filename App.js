0911768ae2b7275c838ae87a4d0b64f2e76a54df
David Kennell <davidkennell@gmail.com>
2019-10-14 11:29:49 -0500

    Add sound and link up sound with onPress event for button image

--- App.js
+++ App.js
@@ -1,17 +1,27 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import { Audio } from 'expo-av';
 
 export default function App() {
+  playSound = async () => {
+    await Audio.setIsEnabledAsync(true);
+    const sound = new Audio.Sound();
+    await sound.loadAsync(require('./assets/sounds/pew-pew.mp3'));
+    await sound.playAsync();
+  }
+
   return (
     <View style={styles.container}>
       <Image
         source={require('./assets/caution.png')}
       />
       <Text>Dangerous laser button!!!!</Text>
-      <Image
-        style={{width: 200, height: 200}}
-        source={require('./assets/laser-button.png')}
-      />
+      <TouchableOpacity onPress={playSound}>
+        <Image
+          style={{width: 200, height: 200}}
+          source={require('./assets/laser-button.png')}
+        />
+      </TouchableOpacity>
       <Text>Dangerous laser button!!!!</Text>
       <Image
         ImageResizeMode="center"
